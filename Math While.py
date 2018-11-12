60c541f1c1a8f873ef4c8bf895c7595101d9452e
dreamosky <dreamosky2012@gmail.com>
2018-11-12 12:50:18 +0900

    Math

---
+++ Math While.py
@@ -0,0 +1,24 @@
+def linearSearch(values, target):
+    for i in range(len(values)):
+        if values[i]==target:
+            return i
+        return -1
+
+from random import randint
+n=20
+values=[]
+for i in range(n):
+    values.append(randint(1,100))
+print(values)
+
+done=False
+while not done:
+    target=int(input("Enter number to search for, -1 to quit:"))
+    if target ==-1:
+        done=True
+    else:
+        pos=linearSearch(values,target)
+        if pos ==-1:
+            print("Not found")
+        else:
+            print("Fond in position", pos)
