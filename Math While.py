a4714b6568011c5f4b1b15fe2e82b9b30ed00b12
dreamosky <dreamosky2012@gmail.com>
2018-11-12 18:04:11 +0900

    Math correct

--- Math While.py
+++ Math While.py
@@ -2,7 +2,7 @@ def linearSearch(values, target):
     for i in range(len(values)):
         if values[i]==target:
             return i
-        return -1
+    return -1
 
 from random import randint
 n=10
